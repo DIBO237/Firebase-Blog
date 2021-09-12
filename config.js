d64e92ddbc14d40d80763aa3a40195d504b1fc99
DIBYO-DEY <63423632+DIBO237@users.noreply.github.com>
2021-09-12 13:26:33 +0530

    Fire Base

---
+++ config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAHxgMXJqHRU5FtlTkxWydczB6XpraBmjo",
+    authDomain: "angry-686ce.firebaseapp.com",
+    projectId: "angry-686ce",
+    storageBucket: "angry-686ce.appspot.com",
+    messagingSenderId: "403925720956",
+    appId: "1:403925720956:web:d42c9a301bc6609dea4ef9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
